2addc52d5613cf5edb736c8faabbb54eacd72597
Sakshi Pachlaniya <sakshipachlaniya@gmail.com>
2024-09-02 22:03:33 +0530

    schema made

--- backend/db.js
+++ backend/db.js
@@ -1,2 +1,15 @@
 //npm i mongoose 
 //making schemas
+
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/taskmate");
+const taskmateschema=mongoose.Schema({
+    title:String,
+    description:String,
+    completed:Boolean
+})
+
+const todo=mongoose.model('todos',taskmateschema);
+module.exports={
+    todo
+}

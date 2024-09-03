f1dc23d36985a2585e7a3eca4ec045f23641d5a1
Sakshi Pachlaniya <sakshipachlaniya@gmail.com>
2024-09-03 22:08:12 +0530

    frontend created

--- backend/types.js
+++ backend/types.js
@@ -4,8 +4,8 @@
 const zod=require("zod");
 //writing the zod schema
 const createTodo=zod.object({
-    title:zod.string(),
-    description:zod.string()
+    title:zod.string().min(1),
+    description:zod.string().min(1)
 })
 
 const updateTodo=zod.object({
